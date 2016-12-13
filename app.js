b4c352a18bd1a4a1134912ae715dcbeab99d9d9e
Rick Arnett <rarnett@appirio.com>
2016-12-13 12:06:14 -0500

    Space comments for eslint

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-//Require'ing both so .defaults will fill in the blanks
-//DO NOT EDIT .defaults - add your specific settings to .env
-//DO NOT CHECK .env into source control!
-//Refer to INSTRUCTIONS.md
+// Require'ing both so .defaults will fill in the blanks
+// DO NOT EDIT .defaults - add your specific settings to .env
+// DO NOT CHECK .env into source control!
+// Refer to INSTRUCTIONS.md
 
 require('dotenv').config({ path: '.env', silent: true });
 require('dotenv').config({ path: '.defaults', silent: true });
@@ -9,4 +9,3 @@ require('dotenv').config({ path: '.defaults', silent: true });
 const app = require('lusa-pkg-web-pattern')({ basePath: __dirname });
 
 app.listen(process.env.PORT || 3000);
-
